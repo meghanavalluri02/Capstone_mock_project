c6b66317e92a85c3822690808f34c48ab1ec94f9
Juhi Choudhary <115893427+Juhi5863@users.noreply.github.com>
2025-06-16 23:47:29 +0530

    Delete script.sh

--- script.sh
+++
@@ -1,7 +0,0 @@
-docker build -t ecommerce-ecr:latest .
-docker tag ecommerce-ecr:latest 713881797197.dkr.ecr.us-east-1.amazonaws.com/ecommerce-ecr:latest
-docker push 713881797197.dkr.ecr.us-east-1.amazonaws.com/ecommerce-ecr:latest
-kubectl delete all --all
-cd k8s/
-kubectl apply -f .
-kubectl get pods
